abcf3999f0543289fcb5dbb0b03da61be8dfac5c
Roman Tarassenko <tarassenko@me.com>
2022-10-01 10:49:53 +0300

    Создан функционал редактирования записи.
    - добавлен шаблон update_record.html с включаймой формой из partial/record_form.html
    - прописан путь в urls.py
    - описано представление update_view в views.py

--- guest_book_app/urls.py
+++ guest_book_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
 
-from guest_book_app.views import index_view, add_view
+from guest_book_app.views import index_view, add_view, update_view
 
 urlpatterns = [
     path("", index_view, name="index"),
     path("add/", add_view, name="add_record"),
+    path("<int:pk>/update", update_view, name="update_record"),
 ]
--- guest_book_app/views.py
+++ guest_book_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 
 from guest_book_app.forms import RecordForm
 from guest_book_app.models import GuestBookRecord, RecordStatus
@@ -26,3 +26,39 @@ def add_view(request):
             return redirect("index")
         else:
             return render(request, "add_record.html", context={"form": form})
+
+
+# def update_view(request, pk):
+#     task = get_object_or_404(Task, pk=pk)
+#     if request.method == 'POST':
+#         form = TaskForm(request.POST)
+#         if form.is_valid():
+#             task.description = form.cleaned_data['description']
+#             task.detailed_description = form.cleaned_data['detailed_description']
+#             task.status = form.cleaned_data['status']
+#             task.deadline = form.cleaned_data['deadline']
+#             task.save()
+#             return redirect('detailed_task', pk=task.pk)
+#     elif request.method == 'GET':
+#         form = TaskForm(initial={
+#             'description': task.description,
+#             'detailed_description': task.detailed_description,
+#             'status': task.status,
+#             'deadline': task.deadline
+#         })
+#         return render(request, 'update_task.html', context={'task': task, 'form': form})
+
+def update_view(request, pk):
+    record = get_object_or_404(GuestBookRecord, pk=pk)
+    print(record)
+    if request.method == "POST":
+        form = RecordForm(request.POST)
+        if form.is_valid():
+            record.author = form.cleaned_data["author"]
+            record.email = form.cleaned_data["email"]
+            record.text = form.cleaned_data["text"]
+            record.save()
+            return redirect("index")
+    elif request.method == "GET":
+        form = RecordForm(instance=record)
+        return render(request, "update_record.html", context={"form": form})
