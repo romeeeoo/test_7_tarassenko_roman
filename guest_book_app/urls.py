0397c2277dbe3e8897eeca3677acc079e42c6fab
Roman Tarassenko <tarassenko@me.com>
2022-10-01 11:36:54 +0300

    Создан функционал удаления записи.
    - добавлен шаблон confirm_delete.html
    - прописаны пути в urls.py
    - описано представление delete_view и confirm_delete.html в views.py

--- guest_book_app/urls.py
+++ guest_book_app/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
 
-from guest_book_app.views import index_view, add_view, update_view
+from guest_book_app.views import index_view, add_view, update_view, delete_view, confirm_delete_view
 
 urlpatterns = [
     path("", index_view, name="index"),
     path("add/", add_view, name="add_record"),
-    path("<int:pk>/update", update_view, name="update_record"),
+    path("<int:pk>/update/", update_view, name="update_record"),
+    path("<int:pk>/delete/", delete_view, name="delete_record"),
+    path("<int:pk>/confirm-delete/", confirm_delete_view, name="confirm_delete_record"),
 ]
